e6f9143b5c970cadb9f8155037b9724dd29aaf6c
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-25 09:46:16 +0200

    adds depencencies

--- src/main/java/com/lichon/eventmanagement/entities/Event.java
+++ src/main/java/com/lichon/eventmanagement/entities/Event.java
@@ -2,52 +2,90 @@ package com.lichon.eventmanagement.entities;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Set;
 
 public class Event extends AbstractEntity {
-	
+
 	private String name;
 	private String descriptiion;
 	private ZonedDateTime startTime;
 	private ZonedDateTime endTime;
 	private ZoneId zoneId;
 	private Boolean started;
-//	private Organizer organizer;
-	
+	private Organizer organizer;
+	private Set<Participant> participants;
+	private Venue venue;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescriptiion() {
 		return descriptiion;
 	}
+
 	public void setDescriptiion(String descriptiion) {
 		this.descriptiion = descriptiion;
 	}
+
 	public ZonedDateTime getStartTime() {
 		return startTime;
 	}
+
 	public void setStartTime(ZonedDateTime startTime) {
 		this.startTime = startTime;
 	}
+
 	public ZonedDateTime getEndTime() {
 		return endTime;
 	}
+
 	public void setEndTime(ZonedDateTime endTime) {
 		this.endTime = endTime;
 	}
+
 	public ZoneId getZoneId() {
 		return zoneId;
 	}
+
 	public void setZoneId(ZoneId zoneId) {
 		this.zoneId = zoneId;
 	}
+
 	public Boolean getStarted() {
 		return started;
 	}
+
 	public void setStarted(Boolean started) {
 		this.started = started;
+	}
+
+	public Organizer getOrganizer() {
+		return organizer;
+	}
+
+	public void setOrganizer(Organizer organizer) {
+		this.organizer = organizer;
+	}
+
+	public Set<Participant> getParticipants() {
+		return participants;
+	}
+
+	public void setParticipants(Set<Participant> participants) {
+		this.participants = participants;
+	}
+
+	public Venue getVenue() {
+		return venue;
+	}
+
+	public void setVenue(Venue venue) {
+		this.venue = venue;
 	}
 
 }
--- src/main/java/com/lichon/eventmanagement/entities/Organizer.java
+++ src/main/java/com/lichon/eventmanagement/entities/Organizer.java
@@ -1,8 +1,11 @@
 package com.lichon.eventmanagement.entities;
 
+import java.util.Set;
+
 public class Organizer extends AbstractEntity {
 
 	private String name;
+	private Set<Event> events;
 
 	public String getName() {
 		return name;
@@ -11,6 +14,14 @@ public class Organizer extends AbstractEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public Set<Event> getEvents() {
+		return events;
+	}
+
+	public void setEvents(Set<Event> events) {
+		this.events = events;
+	}
+
 	
 }
