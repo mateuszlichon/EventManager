1dc4fce1f7a9a2a727c7f1a1dcf0fa07314081ea
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-26 14:51:02 +0200

    develops repositories

--- src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
@@ -1,5 +1,7 @@
 package com.lichon.eventmanagement.repos;
 
+import java.time.ZoneId;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -10,4 +12,6 @@ import com.lichon.eventmanagement.entities.Event;
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
 	Page<Event> findByName(@Param("name") String name, Pageable pageable);
+	
+	Page<Event>findByNameAndZoneId(@Param("name") String name, @Param("zoneId") ZoneId zoneId, Pageable pageable);
 }
--- src/main/java/com/lichon/eventmanagement/repos/VenueRepository.java
+++ src/main/java/com/lichon/eventmanagement/repos/VenueRepository.java
@@ -1,9 +1,14 @@
 package com.lichon.eventmanagement.repos;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.lichon.eventmanagement.entities.Venue;
 
 public interface VenueRepository extends PagingAndSortingRepository<Venue, Long> {
+	
+	Page<Venue> findByPostalCode(@Param("postalcode") String postalcode, Pageable pageable);
 
 }
