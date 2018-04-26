6532ded73324ac726ab939ad7383d2374b548d90
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-26 12:42:57 +0200

    extends repositories with paging and sorting

--- src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
@@ -1,9 +1,9 @@
 package com.lichon.eventmanagement.repos;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.lichon.eventmanagement.entities.Event;
 
-public interface EventRepository extends CrudRepository<Event, Long> {
+public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
 }
--- src/main/java/com/lichon/eventmanagement/repos/OrganizerRepository.java
+++ src/main/java/com/lichon/eventmanagement/repos/OrganizerRepository.java
@@ -1,9 +1,9 @@
 package com.lichon.eventmanagement.repos;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.lichon.eventmanagement.entities.Organizer;
 
-public interface OrganizerRepository extends CrudRepository<Organizer, Long> {
+public interface OrganizerRepository extends PagingAndSortingRepository<Organizer, Long> {
 
 }
