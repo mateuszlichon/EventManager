7f805d6dac3692bd1b14a78fb5f69ad971ca3661
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-26 14:35:05 +0200

    adds pageability

--- src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
@@ -1,13 +1,13 @@
 package com.lichon.eventmanagement.repos;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.lichon.eventmanagement.entities.Event;
-import java.lang.String;
-import java.util.List;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
-	List<Event> findByName(@Param("name") String name);
+	Page<Event> findByName(@Param("name") String name, Pageable pageable);
 }
