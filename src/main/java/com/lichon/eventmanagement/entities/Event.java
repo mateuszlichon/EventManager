f64031499cf4e6b72e0359699a16f01cefaa51da
mateuszlichon <mateusz.lichon@gmail.com>
2018-04-26 13:59:43 +0200

    adds order property

--- src/main/java/com/lichon/eventmanagement/entities/Event.java
+++ src/main/java/com/lichon/eventmanagement/entities/Event.java
@@ -14,6 +14,9 @@ import javax.persistence.OneToMany;
 
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"resourceId"})
 @Entity
 public class Event extends AbstractEntity {
 
