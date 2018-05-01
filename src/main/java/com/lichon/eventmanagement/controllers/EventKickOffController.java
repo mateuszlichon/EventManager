1e88e6fce60e0fefa7cd89b4fa12d0371f59756b
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-01 11:40:16 +0200

    creates start method

--- src/main/java/com/lichon/eventmanagement/controllers/EventKickOffController.java
+++ src/main/java/com/lichon/eventmanagement/controllers/EventKickOffController.java
@@ -1,11 +1,17 @@
 package com.lichon.eventmanagement.controllers;
 
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RepositoryRestController
 @RequestMapping("/events")
 public class EventKickOffController {
 
-	
+	@PostMapping("/start/{id}")
+	public ResponseEntity start(@PathVariable Long id) {
+		return null;
+	}
 }
