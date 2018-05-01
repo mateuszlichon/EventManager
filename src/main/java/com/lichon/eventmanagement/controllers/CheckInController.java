39e5ee415ad77e597bc8173ebfd793c62fcf3ec3
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-01 12:01:28 +0200

    implements check in method

--- src/main/java/com/lichon/eventmanagement/controllers/CheckInController.java
+++ src/main/java/com/lichon/eventmanagement/controllers/CheckInController.java
@@ -1,19 +1,36 @@
 package com.lichon.eventmanagement.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.lichon.eventmanagement.controllers.exceptions.AlreadyCheckedInException;
 import com.lichon.eventmanagement.entities.Participant;
+import com.lichon.eventmanagement.repos.ParticipantRepository;
 
 @RepositoryRestController
 @RequestMapping("/events")
 public class CheckInController {
 
+	@Autowired
+	private ParticipantRepository participantRepository;
+
 	@PostMapping("/checkin/{id}")
 	public ResponseEntity<Participant> checkIn(@PathVariable Long id) {
+
+		Participant participant = participantRepository.findOne(id);
+
+		if (participant != null) {
+			if (participant.getCheckedIn()) {
+				throw new AlreadyCheckedInException();
+			}
+			participant.setCheckedIn(true);
+			participantRepository.save(participant);
+		}
+
 		return null;
 	}
 }
---
+++ src/main/java/com/lichon/eventmanagement/controllers/exceptions/AlreadyCheckedInException.java
@@ -0,0 +1,5 @@
+package com.lichon.eventmanagement.controllers.exceptions;
+
+public class AlreadyCheckedInException extends RuntimeException {
+
+}
