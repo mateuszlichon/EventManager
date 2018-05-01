b88918150cc7267db60ebbf0c0305f7c2025f340
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-01 11:37:43 +0200

    creates event controller

---
+++ src/main/java/com/lichon/eventmanagement/controllers/EventKickOffController.java
@@ -0,0 +1,11 @@
+package com.lichon.eventmanagement.controllers;
+
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RepositoryRestController
+@RequestMapping("/events")
+public class EventKickOffController {
+
+	
+}
