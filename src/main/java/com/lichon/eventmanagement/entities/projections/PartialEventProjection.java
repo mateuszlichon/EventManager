97d8deab79379799fe759e6cae07d0324a087808
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-08 15:43:31 +0200

    adds preauthorization for delete function

--- src/main/java/com/lichon/eventmanagement/entities/projections/PartialEventProjection.java
+++ src/main/java/com/lichon/eventmanagement/entities/projections/PartialEventProjection.java
@@ -17,4 +17,6 @@ public interface PartialEventProjection {
 	ZonedDateTime getEndTime();
 	
 	Instant getCreated();
+	
+	Long getResourceId();
 }
--- src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.lichon.eventmanagement.entities.Event;
 import com.lichon.eventmanagement.entities.projections.PartialEventProjection;
@@ -17,4 +18,8 @@ public interface EventRepository extends PagingAndSortingRepository<Event, Long>
 	Page<Event> findByName(@Param("name") String name, Pageable pageable);
 
 	Page<Event> findByNameAndZoneId(@Param("name") String name, @Param("zoneId") ZoneId zoneId, Pageable pageable);
+	
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	void delete(Long id);
 }
