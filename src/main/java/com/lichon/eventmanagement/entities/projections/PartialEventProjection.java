ff21d7bb870f4e1ec4b9b24075250bc40d770809
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-08 12:30:35 +0200

    introduces abstract methods in projection

--- src/main/java/com/lichon/eventmanagement/entities/projections/PartialEventProjection.java
+++ src/main/java/com/lichon/eventmanagement/entities/projections/PartialEventProjection.java
@@ -1,5 +1,7 @@
 package com.lichon.eventmanagement.entities.projections;
 
+import java.time.ZonedDateTime;
+
 import org.springframework.data.rest.core.config.Projection;
 
 import antlr.debug.Event;
@@ -7,4 +9,9 @@ import antlr.debug.Event;
 @Projection(name = "partial", types = { Event.class })
 public interface PartialEventProjection {
 
+	String getName();
+
+	ZonedDateTime getStartTime();
+
+	ZonedDateTime getEndTime();
 }
