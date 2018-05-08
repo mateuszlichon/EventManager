5e8aef090c666dcf2029fcbb98e7731b3889e10b
mateuszlichon <mateusz.lichon@gmail.com>
2018-05-08 12:46:46 +0200

    sets excerpth for collection

--- src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/lichon/eventmanagement/repos/EventRepository.java
@@ -6,12 +6,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.lichon.eventmanagement.entities.Event;
+import com.lichon.eventmanagement.entities.projections.PartialEventProjection;
 
+@RepositoryRestResource(excerptProjection = PartialEventProjection.class)
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
 	Page<Event> findByName(@Param("name") String name, Pageable pageable);
-	
-	Page<Event>findByNameAndZoneId(@Param("name") String name, @Param("zoneId") ZoneId zoneId, Pageable pageable);
+
+	Page<Event> findByNameAndZoneId(@Param("name") String name, @Param("zoneId") ZoneId zoneId, Pageable pageable);
 }
